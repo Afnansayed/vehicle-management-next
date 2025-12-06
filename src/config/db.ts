906783649ab63a29d39dd3511ac5776554784c21
Afnan Sayed <afnansayed1973@gimal.com>
2025-12-06 16:17:28 +0600

    vehicles db schema

--- src/config/db.ts
+++ src/config/db.ts
@@ -16,6 +16,17 @@ const initDB = async () => {
       role VARCHAR(10) NOT NULL CHECK (role IN ('admin', 'customer'))
     )
     `);
+  
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS vehicles(
+      id SERIAL PRIMARY KEY,
+      vehicle_name VARCHAR(100) NOT NULL,
+      type VARCHAR(10) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'SUV')),
+      registration_number VARCHAR(30) NOT NULL UNIQUE,
+      daily_rent_price NUMERIC(10,2) NOT NULL CHECK (daily_rent_price > 0),
+      availability_status VARCHAR(10) NOT NULL CHECK (availability_status IN ('available', 'booked'))
+      )
+      `)
 };
 
 export default initDB;
